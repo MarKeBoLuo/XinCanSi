02cb7a776131cbdb57be026c7192205a3a132688
982407623@qq.com <yxy1021liuhui>
2019-12-17 14:15:16 +0800

    项目文件初始化

---
+++ src/index.js
@@ -0,0 +1,20 @@
+console.log('hello world');
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div>
+                <p>welcome to React</p>
+            </div>
+        )
+    }
+}
+
+export default App;
+ReactDom.render(<App/>, document.getElementById('root'));
