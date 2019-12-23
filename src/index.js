83ac0751fe8adad5d90eda338f2813ab82e881d0
982407623@qq.com <yxy1021liuhui>
2019-12-23 16:10:45 +0800

    完整的生命周期

--- src/index.js
+++ src/index.js
@@ -5,11 +5,50 @@ import 'antd/dist/antd.css';
 import './assets/scss/index.scss';
 import Home from './pages/login/index'
 
+/**
+ * mount 挂载
+ * update 更新
+ * unmount 卸载
+ */
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
+    /**
+     * componentWillMount
+     * 即将挂载
+     */
+    componentWillMount() {}
+    /**
+     * componentDidMount
+     * 挂载完成
+     */
+    componentDidMount() {}
+    /**
+     * componentWillUpdate
+     * @param {*} nextProps 即将要更新的属性
+     * @param {*} nextState 即将要更新的状态
+     */
+    componentWillUpdate(nextProps, nextState) {}
+    /**
+     * componentDidUpdate
+     * 页面更新完毕
+     * @param {*} prevProps 更改的属性
+     * @param {*} prevState 更改的状态
+     */
+    componentDidUpdate(prevProps, prevState) {}
+    /**
+     * shouldComponentUpdate
+     * 默认每一次的修改都触发页面更新，此函数用于干预是否要更新，用于性能优化
+     */
+    shouldComponentUpdate() {}
+    /**
+     * componentWillUnmount
+     * 卸载
+     */
+    componentWillUnmount() {}
     render() {
         const { Header, Footer, Content } = Layout
         return (
